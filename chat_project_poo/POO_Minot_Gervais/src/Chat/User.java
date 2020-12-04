fc46d4724ff5a237b1ed30d949fb1bba817df1de
Paul <p.gervais17@laposte.net>
2020-12-04 13:42:28 +0100

    push initial

---
+++ chat_project_poo/POO_Minot_Gervais/src/Chat/User.java
@@ -0,0 +1,37 @@
+package Chat;
+
+public class User {
+	private String pseudo;
+	private String password;
+	private Boolean status;
+	
+	public User(String login, String pwd) {
+		// TODO Auto-generated constructor stub
+		this.pseudo=login;
+		this.password=pwd;		
+	}
+	
+	public void Set_Pseudo(String p) {
+		this.pseudo=p;
+	}
+	
+	public void Set_Password(String p) {
+		this.password=p;
+	}
+
+	public void Set_Status(Boolean s) {
+		this.status=s;
+	}
+	
+	public String Get_Pseudo() {
+		return pseudo;
+	}
+	
+	public String Get_Password() {
+		return password;
+	}
+	
+	public Boolean Get_Status() {
+		return status;
+	}
+}
